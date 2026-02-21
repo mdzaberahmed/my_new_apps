b106e6fd8e0566db3eec400d3179faf3373d5ed7
mdzaberahmed <YOUR_GITHUB_EMAIL>
2026-02-22 00:51:50 +0800

    Fixed package name in MainActivity to solve app crashing

--- android/app/src/main/kotlin/com/example/ff_boost_premium/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ff_boost_premium/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.ff_boost_premium
+package com.mdzaberahmed.ffboostpanel
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
